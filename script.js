1268a2f96ccb39569ba053a664e97c12b211b487
Dair Smith <dair.code@gmail.com>
2021-04-26 17:23:10 -0400

    changed order of author and awards

--- script.js
+++ script.js
@@ -28,15 +28,6 @@ for (let child of data.data.children) {
     const top4Award = child.data.all_awardings.slice(0, 4);
     console.log(top4Award);
 
-    for (let medal of top4Award) {
-        const award = document.createElement("img");
-        award.setAttribute("src", medal.icon_url);
-        postAwards.classList.add("awards");
-        award.classList.add("award");
-        postAwards.appendChild(award);
-        postTop.appendChild(postAwards);
-    }
-
     postDiv.classList.add("post");
     parentEl.appendChild(postDiv);
 
@@ -46,6 +37,15 @@ for (let child of data.data.children) {
     postAuthor.innerText = "Posted by " + child.data.author;
     postAuthor.classList.add("author");
     postTop.appendChild(postAuthor);
+
+    for (let medal of top4Award) {
+        const award = document.createElement("img");
+        award.setAttribute("src", medal.icon_url);
+        postAwards.classList.add("awards");
+        award.classList.add("award");
+        postAwards.appendChild(award);
+        postTop.appendChild(postAwards);
+    }
 
     postTitle.innerText = child.data.title;
     postTitle.classList.add("title");
