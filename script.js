482127c5b34b52a97b1598fecbf37e250c0ff8bb
Dair Smith <dair.code@gmail.com>
2021-04-26 16:01:28 -0400

    thumbnail, title, and url

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ fetch("https://www.reddit.com/r/aww/.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-  });
 
 // . (dot) for object properties
 // [] (brackets) for array elements
+
+const parentEl = document.getElementById("postParent");
+
+for (let child of data.data.children) {
+    const postDiv = document.createElement("div");
+    const thumbnail = document.createElement("img");
+    const postTitle = document.createElement("p");
+    const postLink = document.createElement("a");
+    var url = child.data.url;
+    parentEl.appendChild(postDiv);
+    postTitle.innerText = child.data.title;
+    postDiv.appendChild(postTitle);
+    if (child.data.thumbnail === "default") {
+        thumbnail.remove();
+    } else {
+        thumbnail.setAttribute("src", child.data.thumbnail);
+        postDiv.appendChild(thumbnail);
+    }
+    postLink.setAttribute("href", url);
+    postLink.innerText = "See this post on Reddit.";
+    postDiv.appendChild(postLink);
+}
+});
